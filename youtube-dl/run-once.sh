391f0fb3abfb65cbcedb1daac58d5778b29e5d9d
Konstantinos Antoniou <101103222+funmicra@users.noreply.github.com>
2023-11-18 10:53:20 +0200

    Create run-once.sh

---
+++ youtube-dl/run-once.sh
@@ -0,0 +1,6 @@
+ln -s ../docker-env.cfg ./.env
+. ./.env
+mkdir -p ${BASEDIR}/youtubedl/config
+mkdir -p ${BASEDIR}/youtubedl/audio
+mkdir -p ${BASEDIR}/youtubedl/subscriptions
+mkdir -p ${BASEDIR}/youtubedl/users
