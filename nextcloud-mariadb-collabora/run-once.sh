e511ec0d8b029e4aa10a559a05ac6e8df82d8e10
Konstantinos Antoniou <101103222+funmicra@users.noreply.github.com>
2023-11-18 10:14:55 +0200

    Create run-once.sh

---
+++ nextcloud-mariadb-collabora/run-once.sh
@@ -0,0 +1,6 @@
+ln -s ../docker-env.cfg ./.env
+. ./.env
+mkdir -p ${BASEDIR}/nextcloud/config
+mkdir -p ${BASEDIR}/nextcloud/data
+mkdir -p ${BASEDIR}/nextcloud_db/config
+mkdir -p ${BASEDIR}/nextcloud_db/data
