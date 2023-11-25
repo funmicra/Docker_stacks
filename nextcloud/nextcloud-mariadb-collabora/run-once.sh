b2d23f090019267cb59d09e037c585ace1a1b823
funmicra <el.funmicra@gmail.com>
2023-11-25 21:40:23 +0200

    updates

--- nextcloud/nextcloud-mariadb-collabora/run-once.sh
+++
@@ -1,6 +0,0 @@
-ln -s ../docker-env.cfg ./.env
-. ./.env
-mkdir -p ${BASEDIR}/nextcloud/config
-mkdir -p ${BASEDIR}/nextcloud/data
-mkdir -p ${BASEDIR}/nextcloud_db/config
-mkdir -p ${BASEDIR}/nextcloud_db/data
